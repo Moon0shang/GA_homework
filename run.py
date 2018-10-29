98fb100481e15d8adc5058762236eb20798e0609
X <moon0shang@gmail.com>
2018-10-29 22:57:30 +0800

    'adoption_night'

--- run.py
+++ run.py
@@ -5,16 +5,18 @@ from GA_math import GA_core
 
 def run(population, accuracy):
 
-    for i in range(500):
+    neg_min = False
+
+    for i in range(3000):
         y = Ga.fitness(population, i)
-        new_pop = Ga.select(y, population)
+        # 判断最优值正负
+        if np.min(y) < 0:
+            neg_min = True
 
-        pop_max = np.max(new_pop)
-        l_max = len(bin(int(pop_max/accuracy)))-3
-
+        new_pop = Ga.select(y, population, neg_min)
         bin_pop = Ga.encoding(new_pop)
-        cr_pop = Ga.crossover(bin_pop, l_max)
-        mu_pop = Ga.mutation(cr_pop, l_max)
+        cr_pop = Ga.crossover(bin_pop)
+        mu_pop = Ga.mutation(cr_pop)
         population = Ga.decoding(mu_pop)
         # print('generation:'+'%s' % (i+1))
 
@@ -32,16 +34,18 @@ def visualize(i):
              "best value:%s" % np.round(y, 6))
     plt.xlabel('generation')
     plt.ylabel('f(x,y)')
-    plt.savefig('C:/Users/X/Desktop/GA_math/5/%s.png' % i)
+    plt.savefig('C:/Users/X/Desktop/GA_math/11/%s.png' % i)
     plt.close()
 
 
-accuracy = 0.0001
-Ga = GA_core(2, [-5, 5], accuracy)
+accuracy = 0.1
+Ga = GA_core(30, [-100, 100], accuracy)
+
 
 for i in range(30):
     Ga.reset()
-    population = Ga.initial(50, 5)
+    population = Ga.initial(50, 1)
     run(population, accuracy)
     # np.savetxt('C:/Users/X/Desktop/GA_math/2/%s.txt' % i, Ga.best_group_his)
     visualize(i)
+    print('output pic%s' % i)
