9a5044b7fd1e9d927377c35abeb9c1f10c0fa246
X <moon0shang@gmail.com>
2018-10-31 18:25:10 +0800

    'doing'

--- GA_TSP.py
+++ GA_TSP.py
@@ -7,26 +7,61 @@ note: It's a homework of natrue calculation, which need to use GA to solve TSP p
 """
 
 import numpy as np
+from fit_tsp import fit_func
 
 
 class GA_core(object):
 
-    def __init__(self, cross_rate, muta_rate, **args):
+    def __init__(self, cross_rate, muta_rate, group_num, **args):
 
         self.p_c = cross_rate
         self.p_m = muta_rate
+        self.g_num = group_num
+        self.best = 99999999
+        self.best_history = []
 
-    def initail(self):
+    def initail(self, city_num):
         """初始化"""
-        pass
+        population = []
 
-    def fitness(self):
+        for i in range(self.g_num):
+            rand_pop = [j for j in range(city_num)]
+            population.append(np.random.shuffle(rand_pop))
+
+        return population
+
+    def fitness(self, pop):
         """计算适应度"""
-        pass
+        y = np.empty(self.g_num)
+        for i in range(self.g_num):
+            y[i] = fit_func(pop[i])
 
-    def select(self):
+        best = np.min(y)
+        b = np.where(y == best)[0][0]
+        best_group = pop[b]
+
+        if best < self.best:
+            self.best = best
+            self.best_group = best_group
+
+        self.best_history.append(best)
+
+        return y
+
+    def select(self, fit, pop):
         """自然选择"""
-        pass
+        total = np.sum(fit)
+        p0 = fit/total
+        p = np.cumsum(p0)
+        p[-1] = 1
+        c = np.random.rand(self.g_num)
+
+        pop_new = [None]*self.g_num
+        for i in range(self.g_num):
+            s = np.where(p >= c[i])[0][0]
+            pop_new[i] = pop[i]
+
+        return pop_new
 
     def cross(self):
         """交叉"""
