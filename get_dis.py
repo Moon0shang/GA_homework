20f99bb7f22bd3f61ca218b7948b3399f9d57b3f
X <moon0shang@gmail.com>
2018-11-02 00:45:57 +0800

    'modify'

--- get_dis.py
+++ get_dis.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def read_info():
     """读取tsp坐标数据"""
-    with open('./bays29.tsp', 'r') as f:
-        raw = f.read().split('DISPLAY_DATA_SECTION')
+    with open('./ch150.tsp', 'r') as f:
+        raw = f.read().split('NODE_COORD_SECTION')
 
     loc_list = raw[1].split('EOF')[0].replace('\n', '').split(' ')
     loc_valid = list(filter(lambda n: n != '', loc_list))
@@ -12,14 +12,14 @@ def read_info():
     locations = np.array(loc_raw, dtype=np.float32)
     locations = locations.reshape(len(locations) // 2, 2)
 
-    dis = raw[0].split('EDGE_WEIGHT_SECTION')[1]
+    """     dis = raw[0].split('EDGE_WEIGHT_SECTION')[1]
     dis_list = dis.replace('\n', '').split(' ')
     dis_valid = list(filter(lambda n: n != '', dis_list))
     distances = np.array(dis_valid, dtype=np.float32)
     l = locations.shape[0]
-    distances = distances.reshape([l, l])
+    distances = distances.reshape([l, l]) """
 
-    return locations, distances
+    return locations  # , distances
 
 
 def cal_dis(loc):
